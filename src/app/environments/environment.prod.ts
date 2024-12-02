d386e97fd78e9e18eb9665a5f29e86ffbe3f7c43
CristhoperSocalay <115178151+CristhoperSocalayR@users.noreply.github.com>
2024-12-02 07:34:08 +0000

    no funco

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //BACK_URL: 'https://turbo-broccoli-76q7x7rr977crgg5-8085.app.github.dev',
-  BACK_URL: '${BACK_URL}'
+  BACK_URL: 'https://turbo-broccoli-76q7x7rr977crgg5-8085.app.github.dev',
+  //BACK_URL: '${BACK_URL}'
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //BACK_URL: 'https://turbo-broccoli-76q7x7rr977crgg5-8085.app.github.dev',
-  BACK_URL: '${BACK_URL}'
+  // Use variable interpolation to load backend URL dynamically
+  BACK_URL: 'https://turbo-broccoli-76q7x7rr977crgg5-8085.app.github.dev',
 };
 
 /*
